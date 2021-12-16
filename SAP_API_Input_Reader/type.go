e468d0cdbfaa67b4337085d7216d4831bef70a07
Yurino Oga <yurino.o@latona.io>
2021-12-16 09:38:25 +0900

    release

--- SAP_API_Input_Reader/type.go
+++ SAP_API_Input_Reader/type.go
@@ -69,132 +69,79 @@ type SDC struct {
 		SoldToParty                    string `json:"SoldToParty"`
 		CreationDate                   string `json:"CreationDate"`
 		LastChangeDate                 string `json:"LastChangeDate"`
-		ExternalDocumentID             string `json:"ExternalDocumentID"`
-		LastChangeDateTime             string `json:"LastChangeDateTime"`
 		PurchaseOrderByCustomer        string `json:"PurchaseOrderByCustomer"`
+		CustomerPurchaseOrderType      string `json:"CustomerPurchaseOrderType"`
 		CustomerPurchaseOrderDate      string `json:"CustomerPurchaseOrderDate"`
 		SalesInquiryDate               string `json:"SalesInquiryDate"`
 		TotalNetAmount                 string `json:"TotalNetAmount"`
-		OverallDeliveryStatus          string `json:"OverallDeliveryStatus"`
-		TotalBlockStatus               string `json:"TotalBlockStatus"`
-		OverallOrdReltdBillgStatus     string `json:"OverallOrdReltdBillgStatus"`
-		OverallSDDocReferenceStatus    string `json:"OverallSDDocReferenceStatus"`
 		TransactionCurrency            string `json:"TransactionCurrency"`
 		SDDocumentReason               string `json:"SDDocumentReason"`
 		PricingDate                    string `json:"PricingDate"`
-		PriceDetnExchangeRate          string `json:"PriceDetnExchangeRate"`
-		RequestedDeliveryDate          string `json:"RequestedDeliveryDate"`
-		ShippingCondition              string `json:"ShippingCondition"`
-		CompleteDeliveryIsDefined      bool   `json:"CompleteDeliveryIsDefined"`
-		ShippingType                   string `json:"ShippingType"`
 		HeaderBillingBlockReason       string `json:"HeaderBillingBlockReason"`
-		DeliveryBlockReason            string `json:"DeliveryBlockReason"`
+		BindingPeriodValidityStartDate string `json:"BindingPeriodValidityStartDate"`
+		BindingPeriodValidityEndDate   string `json:"BindingPeriodValidityEndDate"`
+		HdrOrderProbabilityInPercent   string `json:"HdrOrderProbabilityInPercent"`
+		ExpectedOrderNetAmount         string `json:"ExpectedOrderNetAmount"`
 		IncotermsClassification        string `json:"IncotermsClassification"`
-		CustomerPriceGroup             string `json:"CustomerPriceGroup"`
-		PriceListType                  string `json:"PriceListType"`
 		CustomerPaymentTerms           string `json:"CustomerPaymentTerms"`
 		PaymentMethod                  string `json:"PaymentMethod"`
-		ReferenceSDDocument            string `json:"ReferenceSDDocument"`
-		CustomerAccountAssignmentGroup string `json:"CustomerAccountAssignmentGroup"`
-		ReferenceSDDocumentCategory    string `json:"ReferenceSDDocumentCategory"`
-		AccountingExchangeRate         string `json:"AccountingExchangeRate"`
-		CustomerGroup                  string `json:"CustomerGroup"`
-		AdditionalCustomerGroup1       string `json:"AdditionalCustomerGroup1"`
-		AdditionalCustomerGroup2       string `json:"AdditionalCustomerGroup2"`
-		AdditionalCustomerGroup3       string `json:"AdditionalCustomerGroup3"`
-		AdditionalCustomerGroup4       string `json:"AdditionalCustomerGroup4"`
-		AdditionalCustomerGroup5       string `json:"AdditionalCustomerGroup5"`
-		CustomerTaxClassification1     string `json:"CustomerTaxClassification1"`
+		OverallSDProcessStatus         string `json:"OverallSDProcessStatus"`
 		TotalCreditCheckStatus         string `json:"TotalCreditCheckStatus"`
-		BillingDocumentDate            string `json:"BillingDocumentDate"`
+		OverallSDDocumentRejectionSts  string `json:"OverallSDDocumentRejectionSts"`
 		HeaderPartner                  struct {
 			PartnerFunction string `json:"PartnerFunction"`
 			Customer        string `json:"Customer"`
 			Supplier        string `json:"Supplier"`
 		} `json:"HeaderPartner"`
 		SalesInquiryItem struct {
-			SalesInquiryItem            string `json:"SalesInquiryItem"`
-			SalesInquiryItemCategory    string `json:"SalesInquiryItemCategory"`
-			SalesInquiryItemText        string `json:"SalesInquiryItemText"`
-			PurchaseOrderByCustomer     string `json:"PurchaseOrderByCustomer"`
-			Material                    string `json:"Material"`
-			MaterialByCustomer          string `json:"MaterialByCustomer"`
-			PricingDate                 string `json:"PricingDate"`
-			BillingPlan                 string `json:"BillingPlan"`
-			RequestedQuantity           string `json:"RequestedQuantity"`
-			RequestedQuantityUnit       string `json:"RequestedQuantityUnit"`
-			OrderQuantityUnit           string `json:"OrderQuantityUnit"`
-			ConfdDelivQtyInOrderQtyUnit string `json:"ConfdDelivQtyInOrderQtyUnit"`
-			ItemGrossWeight             string `json:"ItemGrossWeight"`
-			ItemNetWeight               string `json:"ItemNetWeight"`
-			ItemWeightUnit              string `json:"ItemWeightUnit"`
-			ItemVolume                  string `json:"ItemVolume"`
-			ItemVolumeUnit              string `json:"ItemVolumeUnit"`
-			TransactionCurrency         string `json:"TransactionCurrency"`
-			NetAmount                   string `json:"NetAmount"`
-			MaterialGroup               string `json:"MaterialGroup"`
-			MaterialPricingGroup        string `json:"MaterialPricingGroup"`
-			BillingDocumentDate         string `json:"BillingDocumentDate"`
-			Batch                       string `json:"Batch"`
-			ProductionPlant             string `json:"ProductionPlant"`
-			StorageLocation             string `json:"StorageLocation"`
-			DeliveryGroup               string `json:"DeliveryGroup"`
-			ShippingPoint               string `json:"ShippingPoint"`
-			ShippingType                string `json:"ShippingType"`
-			DeliveryPriority            string `json:"DeliveryPriority"`
-			IncotermsClassification     string `json:"IncotermsClassification"`
-			TaxAmount                   string `json:"TaxAmount"`
-			ProductTaxClassification1   string `json:"ProductTaxClassification1"`
-			MatlAccountAssignmentGroup  string `json:"MatlAccountAssignmentGroup"`
-			CostAmount                  string `json:"CostAmount"`
-			CustomerPaymentTerms        string `json:"CustomerPaymentTerms"`
-			CustomerGroup               string `json:"CustomerGroup"`
-			SalesDocumentRjcnReason     string `json:"SalesDocumentRjcnReason"`
-			ItemBillingBlockReason      string `json:"ItemBillingBlockReason"`
-			WBSElement                  string `json:"WBSElement"`
-			ProfitCenter                string `json:"ProfitCenter"`
-			AccountingExchangeRate      string `json:"AccountingExchangeRate"`
-			ReferenceSDDocument         string `json:"ReferenceSDDocument"`
-			ReferenceSDDocumentItem     string `json:"ReferenceSDDocumentItem"`
-			SDProcessStatus             string `json:"SDProcessStatus"`
-			DeliveryStatus              string `json:"DeliveryStatus"`
-			OrderRelatedBillingStatus   string `json:"OrderRelatedBillingStatus"`
-			ItemPricingElement          struct {
-				PricingProcedureStep           string `json:"PricingProcedureStep"`
-				PricingProcedureCounter        string `json:"PricingProcedureCounter"`
-				ConditionType                  string `json:"ConditionType"`
-				PriceConditionDeterminationDte string `json:"PriceConditionDeterminationDte"`
-				ConditionCalculationType       string `json:"ConditionCalculationType"`
-				ConditionBaseValue             string `json:"ConditionBaseValue"`
-				ConditionRateValue             string `json:"ConditionRateValue"`
-				ConditionCurrency              string `json:"ConditionCurrency"`
-				ConditionQuantity              string `json:"ConditionQuantity"`
-				ConditionQuantityUnit          string `json:"ConditionQuantityUnit"`
-				ConditionCategory              string `json:"ConditionCategory"`
-				PricingScaleType               string `json:"PricingScaleType"`
-				ConditionRecord                string `json:"ConditionRecord"`
-				ConditionSequentialNumber      string `json:"ConditionSequentialNumber"`
-				TaxCode                        string `json:"TaxCode"`
-				ConditionAmount                string `json:"ConditionAmount"`
-				TransactionCurrency            string `json:"TransactionCurrency"`
-				PricingScaleBasis              string `json:"PricingScaleBasis"`
-				ConditionScaleBasisValue       string `json:"ConditionScaleBasisValue"`
-				ConditionScaleBasisUnit        string `json:"ConditionScaleBasisUnit"`
-				ConditionScaleBasisCurrency    string `json:"ConditionScaleBasisCurrency"`
-				ConditionIsManuallyChanged     bool   `json:"ConditionIsManuallyChanged"`
+			SalesInquiryItem              string `json:"SalesInquiryItem"`
+			SalesInquiryItemCategory      string `json:"SalesInquiryItemCategory"`
+			SalesInquiryItemText          string `json:"SalesInquiryItemText"`
+			PurchaseOrderByCustomer       string `json:"PurchaseOrderByCustomer"`
+			Material                      string `json:"Material"`
+			MaterialByCustomer            string `json:"MaterialByCustomer"`
+			RequestedQuantity             string `json:"RequestedQuantity"`
+			RequestedQuantityUnit         string `json:"RequestedQuantityUnit"`
+			ItemOrderProbabilityInPercent string `json:"ItemOrderProbabilityInPercent"`
+			ItemGrossWeight               string `json:"ItemGrossWeight"`
+			ItemNetWeight                 string `json:"ItemNetWeight"`
+			ItemWeightUnit                string `json:"ItemWeightUnit"`
+			ItemVolume                    string `json:"ItemVolume"`
+			ItemVolumeUnit                string `json:"ItemVolumeUnit"`
+			TransactionCurrency           string `json:"TransactionCurrency"`
+			NetAmount                     string `json:"NetAmount"`
+			MaterialGroup                 string `json:"MaterialGroup"`
+			Batch                         string `json:"Batch"`
+			IncotermsClassification       string `json:"IncotermsClassification"`
+			CustomerPaymentTerms          string `json:"CustomerPaymentTerms"`
+			SalesDocumentRjcnReason       string `json:"SalesDocumentRjcnReason"`
+			WBSElement                    string `json:"WBSElement"`
+			SDProcessStatus               string `json:"SDProcessStatus"`
+			ItemPricingElement            struct {
+				PricingProcedureStep        string `json:"PricingProcedureStep"`
+				PricingProcedureCounter     string `json:"PricingProcedureCounter"`
+				ConditionApplication        string `json:"ConditionApplication"`
+				ConditionType               string `json:"ConditionType"`
+				PricingDateTime             string `json:"PricingDateTime"`
+				ConditionCalculationType    string `json:"ConditionCalculationType"`
+				ConditionBaseValue          string `json:"ConditionBaseValue"`
+				ConditionRateValue          string `json:"ConditionRateValue"`
+				ConditionCurrency           string `json:"ConditionCurrency"`
+				ConditionQuantity           string `json:"ConditionQuantity"`
+				ConditionQuantityUnit       string `json:"ConditionQuantityUnit"`
+				ConditionCategory           string `json:"ConditionCategory"`
+				PricingScaleType            string `json:"PricingScaleType"`
+				ConditionRecord             string `json:"ConditionRecord"`
+				ConditionSequentialNumber   string `json:"ConditionSequentialNumber"`
+				TaxCode                     string `json:"TaxCode"`
+				ConditionAmount             string `json:"ConditionAmount"`
+				TransactionCurrency         string `json:"TransactionCurrency"`
+				PricingScaleBasis           string `json:"PricingScaleBasis"`
+				ConditionScaleBasisValue    string `json:"ConditionScaleBasisValue"`
+				ConditionScaleBasisUnit     string `json:"ConditionScaleBasisUnit"`
+				ConditionScaleBasisCurrency string `json:"ConditionScaleBasisCurrency"`
+				ConditionIsManuallyChanged  string `json:"ConditionIsManuallyChanged"`
 			} `json:"ItemPricingElement"`
-			ScheduleLine struct {
-				ScheduleLine                  string `json:"ScheduleLine"`
-				RequestedDeliveryDate         string `json:"RequestedDeliveryDate"`
-				ConfirmedDeliveryDate         string `json:"ConfirmedDeliveryDate"`
-				OrderQuantityUnit             string `json:"OrderQuantityUnit"`
-				ScheduleLineOrderQuantity     string `json:"ScheduleLineOrderQuantity"`
-				ConfdOrderQtyByMatlAvailCheck string `json:"ConfdOrderQtyByMatlAvailCheck"`
-				DeliveredQtyInOrderQtyUnit    string `json:"DeliveredQtyInOrderQtyUnit"`
-				OpenConfdDelivQtyInOrdQtyUnit string `json:"OpenConfdDelivQtyInOrdQtyUnit"`
-				CorrectedQtyInOrderQtyUnit    string `json:"CorrectedQtyInOrderQtyUnit"`
-				DelivBlockReasonForSchedLine  string `json:"DelivBlockReasonForSchedLine"`
-			} `json:"ScheduleLine"`
 		} `json:"SalesInquiryItem"`
 	} `json:"SalesInquiry"`
 	APISchema      string `json:"api_schema"`
