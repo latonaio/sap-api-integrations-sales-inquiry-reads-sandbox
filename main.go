3a2055986d3848df8865edc10c2c92fbf9343589
Yurino Oga <yurino.o@latona.io>
2022-01-27 16:28:47 +0900

    release

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	sap_api_caller "sap-api-integrations-sales-inquiry-reads/SAP_API_Caller"
 	"sap-api-integrations-sales-inquiry-reads/sap_api_input_reader"
 
-	"github.com/latonaio/golang-logging-library/logger"
+	"github.com/latonaio/golang-logging-library-for-sap/logger"
 )
 
 func main() {
